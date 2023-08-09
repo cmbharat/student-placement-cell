52f72281a2ffc6ae7c6a4fac1f92ea5839845857
cmbharat <bharatcm70@gmail.com>
2023-08-09 23:49:14 +0530

    added file structure

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 const express = require("express");
 const port = 8000;
 const path = require("path");
+const db = require("./config/mongoose");
+const Contact = require("./models/student");
 
 const app = express();
 
+var studentList = [
+  { name: "bharat", phone: "8151007904" },
+  { name: "sfsfc", phone: "8446413134" },
+  { name: "sads", phone: "4478889956" },
+  { name: "adewfgd", phone: "9785454454" },
+];
+
 const bodyParser = require("body-parser");
+
+// app.use((req, res, next) => {
+//   console.log("midddle1 ware called");
+//   next();
+// });
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var studentList = [
-  { name: "bharat", age: 32, gender: "male" },
-  { name: "keshav", age: 30, gender: "male" },
-  { name: "giri", age: 29, gender: "female" },
-  { name: "rajesh", age: 28, gender: "female" },
-];
+// app.use((req, res, next) => {
+//   console.log("midddle2 ware called");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   console.log("midddle3 ware called");
+//   next();
+// });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static("assets"));
 
 app.get("/", (req, res) => {
   return res.render("home", {
@@ -30,8 +49,48 @@ app.get("/practice", (req, res) => {
   });
 });
 
-app.post("/create-student", function (req, res) {
-  console.log(req.body);
+app.post("/create-student", async function (req, res) {
+  //   console.log(req.body);
+  //   studentList.push({
+  //     name: req.body.name,
+  //     phone: req.body.phone,
+  //   });
+
+  const newStudent = await Contact.create({
+    name: req.body.name,
+    phone: req.body.phone,
+    // },
+    // function (err, newStudent) {
+    //   if (err) {
+    //     console.log("error in creating a contacts");
+    //     return;
+    //   }
+    //   console.log("newStudent===>", newStudent);
+
+    //   res.redirect("/");
+  });
+
+  console.log("newStudent===>", newStudent);
+
+  res.redirect("/");
+});
+
+/*String params*/
+// app.get("/delete-student/:age", function (req, res) {
+//   let age = req.params.age;
+//   console.log(req.params);
+//   res.redirect("/");
+// });
+
+/*Query params*/
+app.get("/delete-student/", function (req, res) {
+  //   let age = req.params.age;
+  console.log(req.query);
+  let studentIndex = studentList.findIndex(
+    (student) => student.phone == req.query.phone
+  );
+  if (studentIndex != -1) studentList.splice(studentIndex, 1);
+  res.redirect("/");
 });
 
 app.listen(port, (err) => {
