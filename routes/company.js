c7f6b1020fac225d24899f98fb1401e453c3871d
cmbharat <bharatcm70@gmail.com>
2023-10-15 23:33:44 +0530

    Added Authentication to all required routes

--- routes/company.js
+++ routes/company.js
@@ -1,19 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const companyController = require("../controllers/company_controller");
+const passport = require("passport");
 
-router.get("/company-list", companyController.companiesList);
+router.get(
+  "/company-list",
+  passport.checkAuthentication,
+  companyController.companiesList
+);
 
-router.post("/add", companyController.createCompany);
+router.post(
+  "/add",
+  passport.checkAuthentication,
+  companyController.createCompany
+);
 
-router.get("/companyinfo", companyController.companyInfo);
+router.get(
+  "/companyinfo",
+  passport.checkAuthentication,
+  companyController.companyInfo
+);
 
-router.post("/schedule-interview", companyController.scheduleInterview);
+router.post(
+  "/schedule-interview",
+  passport.checkAuthentication,
+  companyController.scheduleInterview
+);
 
-router.get("/companydetails", companyController.companydetails);
+router.get(
+  "/companydetails",
+  passport.checkAuthentication,
+  companyController.companydetails
+);
 
-router.post("/update-status/:id", companyController.updateStatus);
+router.post(
+  "/update-status/:id",
+  passport.checkAuthentication,
+  companyController.updateStatus
+);
 
-router.get("/delete-company", companyController.deleteCompany);
+router.get(
+  "/delete-company",
+  passport.checkAuthentication,
+  companyController.deleteCompany
+);
 
 module.exports = router;
--- routes/student.js
+++ routes/student.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const router = express.Router();
 const studentController = require("../controllers/student_controller");
+const passport = require("passport");
 
-router.get("/student-form", studentController.studentForm);
-router.post("/create-student", studentController.createStudent);
-router.get("/studentinfo", studentController.studentInfo);
-router.get("/delete-student", studentController.deleteStudent);
-router.get("/student-form", studentController.studentForm);
-router.post("/create-student", studentController.createStudent);
-router.post("/update-student", studentController.updateStudent);
+router.get(
+  "/student-form",
+  passport.checkAuthentication,
+  studentController.studentForm
+);
+router.post(
+  "/create-student",
+  passport.checkAuthentication,
+  studentController.createStudent
+);
+router.get(
+  "/studentinfo",
+  passport.checkAuthentication,
+  studentController.studentInfo
+);
+router.get(
+  "/delete-student",
+  passport.checkAuthentication,
+  studentController.deleteStudent
+);
+router.get(
+  "/student-form",
+  passport.checkAuthentication,
+  studentController.studentForm
+);
+router.post(
+  "/create-student",
+  passport.checkAuthentication,
+  studentController.createStudent
+);
+router.post(
+  "/update-student",
+  passport.checkAuthentication,
+  studentController.updateStudent
+);
 
 module.exports = router;
